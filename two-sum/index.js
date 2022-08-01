54aca7d4fd017f77055daf4e7485ffc4584b9a38
nizamuddins <uddinmdnizam61@gmail.com>
2022-08-02 03:53:25 +0530

    leet-code

---
+++ two-sum/index.js
@@ -0,0 +1,21 @@
+
+const num = [3,2,4];
+const target = 6;
+
+function myFunc(array) {
+
+    let output = [];
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            let value = array[i] + array[j];
+            if (value === target && i !== j) {
+                output.push(i);
+                output.push(j);
+                return output;
+            }
+        }
+    }
+}
+
+console.log(myFunc(num))
